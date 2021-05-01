c2d49b676e64c49892fc60d15b4d7aaeb67f2b81
unknown <Mostafa Hares>
2021-05-01 17:10:28 +0200

    Bean Scope singleton & prototype

---
+++ src/spring/core/xmlconfig/BeanScopeDemoApp.java
@@ -0,0 +1,32 @@
+package spring.core.xmlconfig;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class BeanScopeDemoApp {
+
+	public static void main(String[] args) {
+		
+		// load spring configuration file
+        ClassPathXmlApplicationContext context =
+                                      	new	ClassPathXmlApplicationContext("beanScope-applicationContext.xml");		
+		
+		
+		// retrieve bean from spring container
+		Coach theCoach = context.getBean("myCoach",Coach.class);
+		Coach alphaCoach = context.getBean("myCoach",Coach.class);
+        
+		// check if they are the same
+		boolean result = (theCoach==alphaCoach);
+		
+		// print the result 
+		System.out.println("Poiniting to the same object : " + result);
+		System.out.println("Memory location for theCoach :" + theCoach);
+		System.out.println("Memory location for alphaCoach  :" + alphaCoach);
+		
+		// close the context
+		context.close();
+		
+
+	}
+
+}
